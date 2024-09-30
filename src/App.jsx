cfd7e6f18c622275a1028b8dc8b24069d8b7d60f
limitless-developer <dev.programmer.ion@gmail.com>
2024-09-30 17:44:36 +0700

    example code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import 'ion-smile/style.css'
+import ionSmile from 'ion-smile'
+
+const HOST = import.meta.env.VITE_SMILE_HOST
+const TOKEN = import.meta.env.VITE_SMILE_TOKEN
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const config = {
+            credential:{
+                host    : HOST, 
+                token   : TOKEN, 
+                userkey : "user1", 
+                session : "example-session"
+            },
+            dom:{
+                moveable:true // default false
+            }
+        }
+
+
+  useEffect(()=>{
+        const smile = new ionSmile(config);
+        smile.init();
+  
+        smile.on("ready",(e)=>{
+            if(e.module="ai" && e.status){
+                smile.start()
+            }
+        });
+        smile.on("alert",(e)=>{
+            console.log(e);
+            
+            if(e.status){ // status = true, if fraud detected
+                // alert(e.description)
+            }
+        });
+  },[])
+
 
   return (
     <>
